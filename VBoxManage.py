7a9f6e909b4d8f8dec141dcbcfdd23a61fd519f2
ryues0 <141699180+ryues0@users.noreply.github.com>
2023-09-18 12:47:31 +0900

    VBoxManage.py upload
    테스트 완료.
    
    스냅샷 생성 및 롤백은 가상머신을 종료한 후에 실행.
    가상머신 실행 옵션은 <gui, 최소 gui, gui없이 진행> 이렇게 3가지 옵션으로 진행하려고 하였으나, 최소gui 옵션이 작동하지 않아서 삭제.

---
+++ VBoxManage.py
@@ -0,0 +1,74 @@
+import subprocess
+
+# vm 리스트 가져오기
+def list_vm() -> list:
+    vboxmanage_path = "C:\\Program Files\\Oracle\\VirtualBox\\vboxmanage.exe"
+    vboxmanage_cmd = [vboxmanage_path]
+
+    command = "list vms"
+    vboxmanage_cmd = vboxmanage_cmd[0:1] + command.split()
+        
+    result = subprocess.run(vboxmanage_cmd, stdout=subprocess.PIPE, text=True)
+    print(result)
+    
+    return result
+
+# vm 시작
+def start_vm(vm_name) -> bool:
+    vboxmanage_path = "C:\\Program Files\\Oracle\\VirtualBox\\vboxmanage.exe"
+    vboxmanage_cmd = [vboxmanage_path]
+
+    # vm 실행 옵션 선택
+    type = ''
+    type_num = input("Do you want to run a virtual machine with a gui type? <yes/no>\n>> ")
+    if type_num == 'yes':       # gui
+        type = 'gui'
+    else:                   # gui를 띄우지 않음
+        type = 'headless'
+
+    command = f"startvm {vm_name} --type {type}"
+    vboxmanage_cmd = vboxmanage_cmd[0:1] + command.split()
+
+    result = subprocess.run(vboxmanage_cmd, stdout=subprocess.PIPE, text=True)
+    print(result)
+
+    return result.returncode
+
+# vm 종료
+def stop_vm(vm_name) -> bool:
+    vboxmanage_path = "C:\\Program Files\\Oracle\\VirtualBox\\vboxmanage.exe"
+    vboxmanage_cmd = [vboxmanage_path]
+    
+    command = f"controlvm {vm_name} poweroff"
+    vboxmanage_cmd = vboxmanage_cmd[0:1] + command.split()
+
+    result = subprocess.run(vboxmanage_cmd, stdout=subprocess.PIPE, text=True)
+    print(result)
+
+    return result.returncode
+
+# 스냅샷 생성
+def snapshot_vm(vm_name, snapshot_name) -> bool:
+    vboxmanage_path = "C:\\Program Files\\Oracle\\VirtualBox\\vboxmanage.exe"
+    vboxmanage_cmd = [vboxmanage_path]
+
+    command = f"snapshot {vm_name} take {snapshot_name} --live "
+    vboxmanage_cmd = vboxmanage_cmd[0:1] + command.split()
+
+    result = subprocess.run(vboxmanage_cmd, stdout=subprocess.PIPE, text=True)
+    print(result)
+
+    return result.returncode
+
+# 스냅샷 시점으로 롤백
+def rollback_vm(vm_name, snapshot_name) -> bool:
+    vboxmanage_path = "C:\\Program Files\\Oracle\\VirtualBox\\vboxmanage.exe"
+    vboxmanage_cmd = [vboxmanage_path]
+    
+    command = f"snapshot {vm_name} restore {snapshot_name}"
+    vboxmanage_cmd = vboxmanage_cmd[0:1] + command.split()
+
+    result = subprocess.run(vboxmanage_cmd, stdout=subprocess.PIPE, text=True)
+    print(result)
+    
+    return result.returncode
