19c5e498e5d2a72ebf0e05b80ddc3f545bca43cb
yura-Choi <dbfk1207@gmail.com>
2023-09-19 18:28:04 +0900

    Trim the code to match the main

--- VBoxManage.py
+++ VBoxManage.py
@@ -14,20 +14,6 @@ def list_vm() -> list:
     vm_list = parse_vm_list(vm_list)
     return vm_list
 
-# 실행중인 vm 리스트 확인
-def list_runningvms() -> list:
-    vboxmanage_path = "C:\\Program Files\\Oracle\\VirtualBox\\vboxmanage.exe"
-    vboxmanage_cmd = [vboxmanage_path]
-
-    command = "list runningvms"
-    vboxmanage_cmd = vboxmanage_cmd[0:1] + command.split()
-        
-    result = subprocess.run(vboxmanage_cmd, stdout=subprocess.PIPE, text=True)
-    running_vm_list = result.stdot.split("\n")[:-1]
-
-    running_vm_list = parse_running_vm_list(running_vm_list)    
-    return running_vm_list
-
 def parse_vm_list(vm_list) -> list:
     parsed_list = []
     
@@ -40,36 +26,39 @@ def parse_vm_list(vm_list) -> list:
     
     return parsed_list
 
-def parse_running_vm_list(running_vm_list) -> list:
-    parsed_list = []
+# 실행중인 vm 리스트 확인
+def list_runningvms() -> list:
+    vboxmanage_path = "C:\\Program Files\\Oracle\\VirtualBox\\vboxmanage.exe"
+    vboxmanage_cmd = [vboxmanage_path]
 
-    for item in running_vm_list:
-        name_range = list(filter(lambda x: item[x] == '\"', range(len(item))))
-        uid_start = item.index("{")
-        uid_end = item.index("}")
+    command = "list runningvms"
+    vboxmanage_cmd = vboxmanage_cmd[0:1] + command.split()
         
-        parsed_list.append([item[name_range[0]+1:name_range[1]], item[uid_start+1:uid_end]])
+    result = subprocess.run(vboxmanage_cmd, stdout=subprocess.PIPE, text=True)
+    running_vm_list = result.stdot.split("\n")[:-1]
+
+    running_vm_list = parse_running_vm(running_vm_list)    
+    return running_vm_list
+
+def parse_running_vm(vm_list) -> list:
+    parsed_list = []
+    
+    for item in vm_list:
+        name_range = list(filter(lambda x: item[x] == '\"', range(len(item))))
+        
+        parsed_list.append(item[name_range[0]+1:name_range[1]])
     
     return parsed_list
 
 # vm 시작
-def start_vm(vm_name) -> bool:
+def start_vm(vm_name, interface_type) -> bool:
     vboxmanage_path = "C:\\Program Files\\Oracle\\VirtualBox\\vboxmanage.exe"
     vboxmanage_cmd = [vboxmanage_path]
 
-    # vm 실행 옵션 선택
-    type = ''
-    type_num = input("Do you want to run a virtual machine with a gui type? <yes/no>\n>> ")
-    if type_num == 'yes':       # gui
-        type = 'gui'
-    else:                       # gui를 띄우지 않음
-        type = 'headless'
-
-    command = f"startvm {vm_name} --type {type}"
+    command = f"startvm {vm_name} --type {interface_type}"
     vboxmanage_cmd = vboxmanage_cmd[0:1] + command.split()
 
     result = subprocess.run(vboxmanage_cmd, stdout=subprocess.PIPE, text=True)
-    print(result)
 
     return result.returncode
 
@@ -82,7 +71,6 @@ def stop_vm(vm_name) -> bool:
     vboxmanage_cmd = vboxmanage_cmd[0:1] + command.split()
 
     result = subprocess.run(vboxmanage_cmd, stdout=subprocess.PIPE, text=True)
-    print(result)
 
     return result.returncode
 
@@ -106,7 +94,7 @@ def parse_snapshot_list(snapshot_list) -> list:
     for item in snapshot_list:
         separator_index = list(filter(lambda x: item[x] == ':', range(len(item))))
         
-        parsed_list.append([item[separator_index[0]+2:separator_index[1]-6], item[separator_index[1]+1:]])
+        parsed_list.append([item[separator_index[0]+2:separator_index[1]-6], item[separator_index[1]+2:]])
     
     return parsed_list
 
@@ -119,7 +107,6 @@ def snapshot_vm(vm_name, snapshot_name) -> bool:
     vboxmanage_cmd = vboxmanage_cmd[0:1] + command.split()
 
     result = subprocess.run(vboxmanage_cmd, stdout=subprocess.PIPE, text=True)
-    print(result)
 
     return result.returncode
 
@@ -132,7 +119,6 @@ def snapshot_delete(vm_name, snapshot_name) -> bool:
     vboxmanage_cmd = vboxmanage_cmd[0:1] + command.split()
 
     result = subprocess.run(vboxmanage_cmd, stdout=subprocess.PIPE, text=True)
-    print(result)
 
     return result.returncode
 
@@ -145,6 +131,5 @@ def rollback_vm(vm_name, snapshot_name) -> bool:
     vboxmanage_cmd = vboxmanage_cmd[0:1] + command.split()
 
     result = subprocess.run(vboxmanage_cmd, stdout=subprocess.PIPE, text=True)
-    print(result)
     
     return result.returncode
